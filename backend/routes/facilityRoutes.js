5002c1d8a9fb75fa88f0bcb11e575b20ab69efac
GKHJanithri <himayajanithri@gmail.com>
2026-03-06 00:40:22 +0530

    Create facility routes

---
+++ backend/routes/facilityRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/facilityController");
+
+router.get("/facilities", controller.getFacilities);
+router.post("/facilities", controller.addFacility);
+router.post("/book", controller.bookFacility);
+router.get("/bookings", controller.getBookings);
+router.delete("/booking/:id", controller.cancelBooking);
+
+module.exports = router;
