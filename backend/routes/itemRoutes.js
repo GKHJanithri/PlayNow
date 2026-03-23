805169b6b77c3f26e6de3d03803b63faa5e26a2b
dewmigithubdewmi <dewmithakshala@gmail.com>
2026-03-24 02:20:24 +0530

    CRUD update 2

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-//Insert the model
-const Item = require("../models/itemModel");
 
 //insert the controller
 const itemControllers = require("../controllers/itemControllers");
 
-router.get("/", itemControllers.getAllItems);
+router.get("/", itemControllers.getItems);
 router.post("/", itemControllers.createItem);
 router.put("/:id", itemControllers.updateItem);
 router.delete("/:id", itemControllers.deleteItem);
-
-
-
+router.post("/:id/reserveItem", itemControllers.reserveItem);
+router.post("/:id/returnItem", itemControllers.returnItem); 
+router.delete("/:id/cancelreservedItem", itemControllers.cancelreservedItem);
+router.get("/:id/returnBookedItem", itemControllers.returnBookedItem);
+router.get("/:id/bookedItem", itemControllers.getBookedItem);
 
 //export the router
 module.exports = router;
