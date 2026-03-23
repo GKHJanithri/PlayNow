2e3d03b7ae61271b78585d692a9275d943927fe4
dewmigithubdewmi <dewmithakshala@gmail.com>
2026-03-24 02:45:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const itemRoutes = require("./routes/itemRoutes");
-const reservationRoutes = require("./routes/itemReservationRoutes");
-const returnRoutes = require("./routes/itemReturnRoutes");
 const app = express();
 
 // Middleware
-app.use("/items", itemRoutes);
-app.use("/reservations", reservationRoutes);
-app.use("/returns", returnRoutes);
 app.use(express.json());
-app.use(cors());  
+app.use(cors());
+app.use("/items", itemRoutes);
 
 // Health check route
 app.get("/", (req, res) => {
